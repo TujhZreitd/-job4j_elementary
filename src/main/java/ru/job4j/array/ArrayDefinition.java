5d94fda0701740f0f9f1e5e1f9b20c608c38ba98
Egor Yakushev <TujhZreited@>
2023-10-18 22:00:35 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println(age.length);
         System.out.println(surname.length);
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Egor";
+        names[1] = "Anton";
+        names[2] = "Alexey";
+        names[3] = "Kirill";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
